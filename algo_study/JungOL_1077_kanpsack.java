62b485f61288afcc49e13231c0c3a22083657f9c
simba155korea <simba155korea@gmail.com>
2021-09-15 15:08:09 +0900

    dp, 배낭문제

---
+++ algo_study/JungOL_1077_kanpsack.java
@@ -0,0 +1,51 @@
+package solve;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class JungOL_1077_kanpsack {
+
+	static int[] weight;
+	static int[] value;
+	
+	static int[] D;
+	
+	public static void main(String[] args) throws IOException{
+		// TODO Auto-generated method stub
+		
+		BufferedReader br1 = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer st1 = new StringTokenizer(br1.readLine());
+		
+		int n = Integer.parseInt(st1.nextToken());
+		int w = Integer.parseInt(st1.nextToken());
+		
+		weight = new int[n];
+		value = new int[n];
+		D = new int[w+1];
+		
+		for(int a=0; a<n; a++)
+		{
+			st1 = new StringTokenizer(br1.readLine());
+			weight[a] = Integer.parseInt(st1.nextToken());
+			value[a] = Integer.parseInt(st1.nextToken());
+		}
+		
+		
+		for(int a=0; a<=w; a++)
+		{
+			for(int b=0; b<n; b++)
+			{
+				if(weight[b]>a) continue;
+				
+				int temp = D[a -weight[b]] + value[b];
+				if(temp > D[a]) D[a] =temp;
+			}
+		}
+
+		System.out.println(D[w]);
+	}
+
+}
